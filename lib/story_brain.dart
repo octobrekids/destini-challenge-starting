f8bb3c2b1880171030b47f8d87b3361d1adc2007
octobrekids <chnrd.k@gmail.com>
2021-06-07 17:35:48 +0700

    fix restart on storynumber 3 4 5

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -65,7 +65,13 @@ class StoryBrain {
       } else if (choiceNumber == 2) {
         _storyNumber = 4;
       }
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
+      restart();
     }
+  }
+
+  void restart() {
+    _storyNumber = 0;
   }
 }
 
