f9d400d3fc8a91546b19cc1209c7298bb07afab5
octobrekids <chnrd.k@gmail.com>
2021-06-07 16:35:51 +0700

    add getChoice2

--- lib/main.dart
+++ lib/main.dart
@@ -70,8 +70,7 @@ class _StoryPageState extends State<StoryPage> {
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: FlatButton(
                   onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                    storyBrain.getChoice2();
                   },
                   color: Colors.blue,
                   child: Text(
